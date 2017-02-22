a797ff6304f8e05a132de4bc60ed2f8b326f73be
Sherrod Blankner <mooncalf@Mooncalf.local>
2017-02-22 13:02:08 -0800

    got snippets working with API viewset

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from rest_framework import routers
-from quickstart import views
+#from quickstart import views
+from snippets import views
 
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+#router.register(r'users', quickstart.views.UserViewSet)
+#router.register(r'groups', quickstart.views.GroupViewSet)
+router.register(r'snippets', views.SnippetViewSet)
 
 
 # Wire up our API using automatic URL routing.
