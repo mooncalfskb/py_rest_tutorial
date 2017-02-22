3fcb9ec3243f96a72bd019a67cc0267cd3c80e63
Sherrod Blankner <mooncalf@Mooncalf.local>
2017-02-22 12:36:34 -0800

    got quickstart working with mysql

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -18,12 +18,12 @@ from django.contrib import admin
 from rest_framework import routers
 from quickstart import views
 
-"""
+
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
-"""
-"""
+
+
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
@@ -35,3 +35,4 @@ urlpatterns = [
 urlpatterns = [
     url(r'^', include('snippets.urls')),
 ]
+"""
