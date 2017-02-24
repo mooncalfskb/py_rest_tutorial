cb69572791aaa5d0aa63784fce9613714dc0f3c7
Sherrod Blankner <mooncalf@Mooncalf.local>
2017-02-23 21:31:14 -0800

    got working without view groups

--- snippets/views.py
+++ snippets/views.py
@@ -14,12 +14,20 @@ from snippets.serializers import SnippetSerializer
 from snippets.serializers import UserSerializer
 from rest_framework import permissions
 from snippets.permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import api_view
+from rest_framework.reverse import reverse
 
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     
     def perform_create(self, serializer):
     	serializer.save(owner=self.request.user)
@@ -38,17 +46,15 @@ class UserDetail(generics.RetrieveAPIView):
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
 
+"""
 class UserViewSet(viewsets.ModelViewSet):
-	"""
-	API endpoint that allows users to be viewed or edited.
-    """
+	#API endpoint that allows users to be viewed or edited.
 	queryset = User.objects.all().order_by('-date_joined')
 	serializer_class = UserSerializer
 
 class SnippetViewSet(viewsets.ModelViewSet):
-	"""
-	API endpoint that allows users to be viewed or edited.
-	"""
+	#API endpoint that allows users to be viewed or edited.
 	queryset = Snippet.objects.all()
 	serializer_class = SnippetSerializer
 	permission_classes = (permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly,)
+"""	
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -17,19 +17,20 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from rest_framework import routers
 #from quickstart import views
-from snippets import views
+#from snippets import views
 
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
+#router = routers.DefaultRouter()
+#router.register(r'users', views.UserViewSet)
 #router.register(r'groups', quickstart.views.GroupViewSet)
-router.register(r'snippets', views.SnippetViewSet)
+#router.register(r'snippets', views.SnippetViewSet)
 
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
+# url(r'^', include(router.urls)),
+
 urlpatterns = [
-    url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^', include('snippets.urls')),
 ]
